d771ae7486816ddcc4134a93e5a80204373da096
exlzseo-coder <exlz.seo@gmail.com>
2025-08-07 15:04:37 +0530

    Add files via upload

---
+++ loader.js
@@ -0,0 +1,6 @@
+(function () {
+  const script = document.createElement("script");
+  script.src = "https://exlzseo-coder.github.io/excelize-ai-chatbot/chatbot.js";
+  script.defer = true;
+  document.body.appendChild(script);
+})();
